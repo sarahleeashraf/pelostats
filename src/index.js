294d232e56bf62f1e21d410d9f2381ab2cdcceb8
SARAH ASHRAF <sarahleeashraf@gmail.com>
2017-08-07 21:53:38 -0400

    add d3 and add some code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import papaparse from 'papaparse';
+import Papa from 'papaparse';
+import d3 from 'd3';
 
-console.log(papaparse);
+console.log("Hello world");
 
+var button = document.querySelector('.js-upload-data');
+var fileInput = document.querySelector('.js-csv-input');
+
+
+button.addEventListener("click", function() {
+    console.log('clickity');
+
+    console.log(fileInput);
+    console.log(fileInput.files);
+
+    if (fileInput.files.length) {
+        var file = fileInput.files[0];
+
+        Papa.parse(file, {
+            complete: function(results, file) {
+                console.log(results.data);
+                console.log(file);
+            },
+            header: true
+        })
+
+
+
+    }
+});
